daa62e5de518015ccb3b84c89bd67a846ee8b613
Lyrth <defelixanne@gmail.com>
2018-10-24 19:59:34 +0800

    Add HTTP fetcher and invite resolver.

--- src/main/java/net/ddns/lyr/utils/Fetch.java
+++ src/main/java/net/ddns/lyr/utils/Fetch.java
@@ -1,10 +1,30 @@
 package net.ddns.lyr.utils;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import discord4j.core.object.entity.Member;
 import discord4j.core.object.entity.User;
 import discord4j.core.object.util.Image;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Optional;
+
 
 public class Fetch {
+
+    private static final Gson gson = new GsonBuilder().create();
+
+
     public static String fetchAvatar(Member u){
         return u.getAvatarUrl(Image.Format.WEB_P).orElse(
             u.getAvatarUrl(Image.Format.GIF).orElse(
@@ -22,5 +42,41 @@ public class Fetch {
                         )
                 )
         );
+    }
+
+
+    public static Optional<String> fetchHttp(String url){
+        try {
+            StringBuilder result = new StringBuilder();
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            try (CloseableHttpResponse response = httpClient.execute(new HttpGet(url))) {
+                HttpEntity entity = response.getEntity();
+                String line;
+                try(BufferedReader rd = new BufferedReader(new InputStreamReader(entity.getContent()))){
+                    while ((line = rd.readLine()) != null) result.append(line);
+                }
+            }
+            return Optional.of(result.toString());
+        } catch (Exception e){
+            Log.logError(e);
+            return Optional.empty();
+        }
+    }
+
+    public static <T> Optional<T> fetchJson(String url, Class<T> clazz){
+        return fetchHttp(url)
+            .map(s -> gson.fromJson(s, clazz));
+    }
+
+
+    public static boolean isInvite(String inviteCode, boolean onlyCheckNonVanity){
+        String link = "http://www.discordapp.com/api/v6/invite/%s?with_counts=true";
+        return fetchJson(String.format(link,inviteCode),InviteObject.class).map( inviteObject ->
+            !(
+                (inviteObject.message != null && inviteObject.message.matches(".*?Unknown Invite.*?")) ||
+                (inviteObject.guild == null) ||
+                (onlyCheckNonVanity && inviteObject.guild.features.contains("VANITY_URL"))
+            )
+        ).orElse(false);
     }
 }
